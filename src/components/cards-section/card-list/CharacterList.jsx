708c1a1dc7005c82d60c5c61b38df96b61398f87
AntonAkhankov <anton.akhankov@gmail.com>
2024-02-20 15:04:05 +0100

    refactor CharacherList component

--- src/components/cards-section/card-list/CharacterList.jsx
+++ src/components/cards-section/card-list/CharacterList.jsx
@@ -1,11 +1,9 @@
-import { Component } from 'react';
 import CharacterCard from '../card-item/CharacterCard';
 import CharacterData from '../../../data/CharacterData';
 import PropTypes from 'prop-types';
 import './CharacterList.css';
-class CharacterList extends Component {
-  render() {
-    const { searchInput } = this.props;
+export default function CharacterList(props){
+    const { searchInput } = props;
     const filteredCharacters = CharacterData.filter((character) => {
       const nameLowerCase = character.name.toLowerCase();
       const searchInputLowerCase = searchInput?.toLowerCase();
@@ -22,10 +20,8 @@ class CharacterList extends Component {
         ))}
       </div>
     );
-  }
 }
 CharacterList.propTypes = {
   searchInput: PropTypes.string,
 };
 
-export default CharacterList;
