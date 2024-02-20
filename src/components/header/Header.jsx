0ecea2bcae8ce641864f649ad292065d0ff686d4
AntonAkhankov <anton.akhankov@gmail.com>
2024-02-20 15:09:56 +0100

    refactor Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,14 @@
-import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-class Header extends Component {
-  render() {
-    return (
-      <header>
-        <p>Vention</p>
-        <Link to="/">
-          <img src="./src/assets/logo.svg" alt="" />
-        </Link>
-        <p>Cards</p>
-      </header>
-    );
-  }
+
+export default function Header() {
+  return (
+    <header>
+      <p>Vention</p>
+      <Link to="/">
+        <img src="./src/assets/logo.svg" alt="" />
+      </Link>
+      <p>Cards</p>
+    </header>
+  );
 }
-export default Header;
